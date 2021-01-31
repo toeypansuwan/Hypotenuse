f8a88401b9fefae0d96f00e8cce27bcf4585fc82
toeypansuwan <70900178+toeypansuwan@users.noreply.github.com>
2021-01-31 12:11:42 +0700

    iii.

--- Hypotenuse/app/src/main/java/th/ac/su/chanchon/hypotenuse/MainActivity.kt
+++ Hypotenuse/app/src/main/java/th/ac/su/chanchon/hypotenuse/MainActivity.kt
@@ -2,10 +2,29 @@ package th.ac.su.chanchon.hypotenuse
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var out = findViewById<TextView>(R.id.out)
+        var a = findViewById<EditText>(R.id.a)
+        var b = findViewById<EditText>(R.id.b)
+        val btn = findViewById<Button>(R.id.btn)
+
+
+        btn.setOnClickListener() {
+            var a1: Double = a.text.toString().toDouble()
+            var b1: Double = b.text.toString().toDouble()
+            var ans = (a1 * a1  + b1*b1)
+            out.text = "C = "+ ans.toString()
+        }
+
     }
+
+
 }
